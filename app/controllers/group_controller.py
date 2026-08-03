900a1982e144c8525ec4abeb67d0b429370b1489
Dima <belousdima777@yandex.ru>
2026-08-03 23:47:31 +0300

    feat: The CRUD operations of group and product have been added.

--- app/controllers/group_controller.py
+++ app/controllers/group_controller.py
@@ -1,21 +1,110 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
 from ..models.group import Group
+from ..models.product import Product
+from ..extensions import db
 
 group_controller = Blueprint('group', __name__)
 
 @group_controller.get('/groups')
+# @jwt_required()
 def get_groups():
+    # need to add checking right of user
+
     groups = Group.query.all()
-    return jsonify({'name': 'hello get'})
+
+    return jsonify([{'id': g.id, 'name': g.name, 'parent_id': g.parent_id} for g in groups])
 
 @group_controller.post('/group')
+# @jwt_required()
 def add_group():
-    return jsonify({'name': 'hello post'})
+    try:
+        data = request.get_json()
 
-@group_controller.put('/group')
-def update_group():
-    return jsonify({'name': 'hello put'})
+        # need to add checking right of user
 
-@group_controller.delete('/group')
-def delete_group():
-    return jsonify({'name': 'hello delete'})
+        if not data or 'name' not in data or 'parent_id' not in data:
+            return jsonify({"error": "Bad Request"}), 400
+
+        group = Group(name = data['name'], parent_id = data['parent_id'])
+
+        db.session.add(group)
+
+        db.session.commit()
+
+        return jsonify({'id': group.id, 'name': group.name, 'parent_id': group.parent_id}), 201
+    except:
+        db.session.rollback()
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+@group_controller.put('/group/<int:id>')
+# @jwt_required()
+def update_group(id):
+    try:
+        group = db.session.get(Group, id)
+
+        if not group:
+            return jsonify({"error": "Not found"}), 404
+
+        # need to add checking right of user
+
+        data = request.get_json()
+
+        if not data:
+            return jsonify({"error": "Bad Request"}), 400
+        
+        if 'name' in data:
+            group.name = data['name']
+        
+        if 'parent_id' in data:
+            group.parent_id = data['parent_id']
+        
+        db.session.commit()
+
+        return jsonify({'id': group.id, 'name': group.name, 'parent_id': group.parent_id}), 200
+    except:
+        db.session.rollback()
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+@group_controller.delete('/group/<int:id>')
+# @jwt_required()
+def delete_group(id):
+    try:
+        group = db.session.get(Group, id)
+
+        if not group:
+            return jsonify({"error": "Not found"}), 404
+
+        # need to add checking right of user
+
+        groups_ids_to_remove = get_all_descendant_groups(group.id)
+
+        products_to_remove = Product.query.filter(Product.group_id.in_(groups_ids_to_remove)).all()
+
+        for p in products_to_remove:
+            db.session.delete(p)
+        
+        groups_to_remove_objects = Group.query.filter(Group.id.in_(groups_ids_to_remove)).all()
+        for g in groups_to_remove_objects:
+            db.session.delete(g)
+
+        db.session.commit()
+
+        return jsonify({'message': 'The item was successfully deleted'}), 200
+    except:
+        db.session.rollback()
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+def get_all_descendant_groups(group_id):
+    descendant_groups_ids = set()
+    groups_to_process = [group_id]
+
+    while groups_to_process:
+        current_group_id = groups_to_process.pop()
+        descendant_groups_ids.add(current_group_id)
+
+        subgroups = Group.query.filter_by(parent_id=current_group_id).all()
+        for subgroup in subgroups:
+            groups_to_process.append(subgroup.id)
+            
+    return list(descendant_groups_ids)
--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -1,21 +1,102 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from ..models.group import Group
 from ..models.product import Product
+from ..extensions import db
 
 product_controller = Blueprint('product', __name__)
 
 @product_controller.get('/products')
-def get_products():
-    products = Product.query.all()
-    return jsonify({'name': 'hello get'})
+@product_controller.get('/products/<int:group_id>')
+def get_products(group_id=None):
+    if group_id is None:
+        all_products = Product.query.all()
+        return jsonify([{'id': p.id, 'name': p.name, 'group_id': p.group_id} for p in all_products])
+
+    group = Group.query.get(group_id)
+
+    if not group:
+        return jsonify({'error': 'Group not found'}), 404
+    
+    products = Product.query.filter(Product.group_id == group_id).all()
+    sub_groups = Group.query.filter(Group.parent_id == group_id).all()
+
+    data = {
+        'products': products,
+        'sub_groups': sub_groups
+    }
+
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({'message': 'No products or sub-groups found for this region'}), 404
 
 @product_controller.post('/product')
 def add_product():
-    return jsonify({'name': 'hello post'})
+    try:
+        data = request.get_json()
 
-@product_controller.put('/product')
-def update_product():
-    return jsonify({'name': 'hello put'})
+        # need to add checking right of user
 
-@product_controller.delete('/product')
-def delete_product():
-    return jsonify({'name': 'hello delete'})
+        if not data or 'name' not in data or 'group_id' not in data:
+            return jsonify({"error": "Bad Request"}), 400
+        
+        if data['group_id'] is None:
+            return jsonify({"error": "Bad Request"}), 400
+
+        product = Product(name = data['name'], group_id = data['group_id'])
+
+        db.session.add(product)
+
+        db.session.commit()
+
+        return jsonify({'id': product.id, 'name': product.name, 'group_id': product.group_id}), 201
+    except:
+        db.session.rollback()
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+@product_controller.put('/product/<int:id>')
+def update_product(id):
+    try:
+        product = db.session.get(Product, id)
+
+        if not product:
+            return jsonify({"error": "Not found"}), 404
+
+        # need to add checking right of user
+
+        data = request.get_json()
+
+        if not data:
+            return jsonify({"error": "Bad Request"}), 400
+        
+        if 'name' in data:
+            product.name = data['name']
+        
+        if 'group_id' in data:
+            product.group_id = data['group_id']
+        
+        db.session.commit()
+
+        return jsonify({'id': product.id, 'name': product.name, 'group_id': product.group_id}), 200
+    except:
+        db.session.rollback()
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+@product_controller.delete('/product/<int:id>')
+def delete_product(id):
+    try:
+        product = db.session.get(Product, id)
+
+        # need to add checking right of user
+
+        if not product:
+            return jsonify({"error": "Not found"}), 404
+        
+        db.session.delete(product)
+
+        db.session.commit()
+
+        return jsonify({'message': 'The item was successfully deleted'}), 200
+    except:
+        db.session.rollback()
+        return jsonify({'error': 'Internal Server Error'}), 500
