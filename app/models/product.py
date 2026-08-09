94d4cf767b47d5cd9340e8ddab65b1b5ffcf8167
Dima <belousdima777@yandex.ru>
2026-08-09 19:47:25 +0300

    fix: The unique name model of product has been changed False.

--- app/models/product.py
+++ app/models/product.py
@@ -2,5 +2,5 @@ from ..extensions import db
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
